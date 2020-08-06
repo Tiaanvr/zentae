ac2e4c4016da970c8fa10f36824a58b4b2ee6199
Tiaan van Rooyen <tiaanvrfrederik@gmail.com>
2020-08-06 19:17:30 +0200

    latest changes as anrich requested

--- src/app/home-content/home-content.component.ts
+++ src/app/home-content/home-content.component.ts
@@ -11,33 +11,33 @@ export class HomeContentComponent implements OnInit {
   burgerClicked = false;
   services = [
     {
-      image: '../../assets/icons/group-37.png',
+      image: '../../assets/icons/group-37.svg',
       title: 'Project Management',
       paragrath: 'ZENTAE Consulting leverages off its years of local and international experience to ensure successful project implementations within accelerated time frames.',
     },
     {
-      image: '../../assets/icons/group-19.png',
+      image: '../../assets/icons/group-19.svg',
       title: 'SAP Software Solutions',
       paragrath: 'ZENTAE Consulting is a certified implementation and value added partner for SAP Concur Travel, Expense and Invoice.',
     },
     {
-      image: '../../assets/icons/group-27.png',
+      image: '../../assets/icons/group-27.svg',
       title: 'Integration',
       paragrath: 'Whether it is cloud or on-premise, ZENTAE Consulting will ensure that your organization’s integration landscape is ready for its SAP journey.',
     },
     {
-      image: '../../assets/icons/group-25.png',
+      image: '../../assets/icons/group-25.svg',
       title: 'Managed Services',
       paragrath: 'Our proven managed services framework is built on the principles of customer satisfaction, data transparency, lean & efficient processes and ensuring best practices.',
     },
     {
-      image: '../../assets/icons/group-29.png',
+      image: '../../assets/icons/group-29.svg',
       title: 'Training & Change Management',
       paragrath: 'ZENTAE Consulting offers bespoke training and change management initiatives to ensure a successful go-live for all end-users.',
     },
     {
-      image: '../../assets/icons/group-18.png',
-      title: 'Business Improvement & Process Optimization',
+      image: '../../assets/icons/group-18.svg',
+      title: 'Business Process Optimization',
       paragrath: 'ZENTAE Consulting will evaluate your new solution’s entire value chain to ensure processes are optimized for maximum ROI.',
     },
   ]
